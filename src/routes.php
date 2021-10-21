4b2649f1bd3afe5e17798965db81a7b1e6f36a1a
Kenny Yeung <kenny.yeung@kanhan.com>
2021-10-22 07:35:21 +0800

    add get route for acs

--- src/routes.php
+++ src/routes.php
@@ -8,37 +8,43 @@ Route::group([
 
     Route::get('/logout', array(
         'as' => 'saml_logout',
-        'https' => 'true', 
+        'https' => 'true',
         'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@logout',
     ));
 
     Route::get('/login', array(
         'as' => 'login',
-        'https' => 'true', 
+        'https' => 'true',
         'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@login',
     ));
 
     Route::get('/sso', array(
         'as' => 'saml_sso_form',
-        'https' => 'true', 
+        'https' => 'true',
         'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@samlCapture',
     ));
 
     Route::get('/metadata', array(
         'as' => 'saml_metadata',
-        'https' => 'true', 
+        'https' => 'true',
         'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@metadata',
     ));
 
     Route::post('/acs', array(
         'as' => 'saml_acs',
-        'https' => 'true', 
+        'https' => 'true',
+        'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@acs',
+    ));
+
+    Route::get('/acs', array(
+        'as' => 'saml_acs',
+        'https' => 'true',
         'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@acs',
     ));
 
     Route::get('/sls', array(
         'as' => 'saml_sls',
-        'https' => 'true', 
+        'https' => 'true',
         'uses' => 'SamlPost\Saml2\Http\Controllers\Saml2Controller@sls',
     ));
 });
